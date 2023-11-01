954b1b82e770e2925edf397f86543ed671f9389d
janneth santos <1722110301@utectulancingo.edu.mx>
2023-11-01 00:45:32 +0000

    Raa

---
+++ static/js/get_all.js
@@ -0,0 +1,34 @@
+function getAll() {
+    var request = new XMLHttpRequest();
+    //request.open('GET', "http://localhost:8000/contactos");
+    request.open('GET', 'https://heroku')
+    request.send();
+
+    request.onload = (e) => {
+        const response = request.responseText;
+        const json = JSON.parse(response);
+        console.log("status_code: " + request.status);
+        const tbody_contactos = document.getElementById("tbody_contactos");
+
+        tbody_contactos.innerHTML = "";
+
+        for (let i = 0; i < json.length; i++) {
+            const contacto = json[i];
+
+            var tr = document.createElement("tr");
+            var td_email = document.createElement("td");
+            var td_nombre = document.createElement("td");
+            var td_telefono = document.createElement("td");
+
+            td_email.innerHTML = contacto["email"];
+            td_nombre.innerHTML = contacto["nombre"];
+            td_telefono.innerHTML = contacto["telefono"];
+
+            tr.appendChild(td_email);
+            tr.appendChild(td_nombre);
+            tr.appendChild(td_telefono);
+
+            tbody_contactos.appendChild(tr);
+        }
+    }
+}
