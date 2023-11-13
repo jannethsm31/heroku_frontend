ffcde64ab38466ec07c4bd51e8c87ec76c2ac366
jannethsm31 <1722110301@utectulancingo.edu.mx>
2023-11-12 21:52:49 -0600

    One

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,8 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const email = urlParams.get('email');
-// Función para obtener un solo registro por su ID
 function getContactById(email) {
-    // Realiza una solicitud para obtener el registro por su ID, por ejemplo:
+
     const request = new XMLHttpRequest();
     //request.open('GET', "http://localhost:8000/contactos/" + email);
     request.open('GET', "https://herokubackend-605c0ee15b4e.herokuapp.com/contactos" + email);
@@ -12,7 +11,6 @@ function getContactById(email) {
         const response = request.responseText;
         const contacto = JSON.parse(response);
 
-        // Ahora puedes mostrar los datos del registro en la página "ver.html"
         const detalle = document.getElementById("detalle");
         detalle.innerHTML = `
             <p>Email: ${contacto.email}</p>
@@ -22,6 +20,5 @@ function getContactById(email) {
     };
 }
 
-// Llama a la función para obtener y mostrar el registro
 getContactById(email);
 
