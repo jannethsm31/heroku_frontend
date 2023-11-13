b49b1e3a3b173e22cc372c92165cb417b6613898
jannethsm31 <1722110301@utectulancingo.edu.mx>
2023-11-12 22:04:52 -0600

    One

--- static/js/get_one.js
+++ static/js/get_one.js
@@ -2,7 +2,7 @@ function getForEmail() {
     var email = document.getElementById("email").value;
     var request = new XMLHttpRequest();
     // var url = "http://localhost:8000/contactos/" + encodeURIComponent(email);
-    var url = "https://herokubackend-605c0ee15b4e.herokuapp.com/contactos" + encodeURIComponent(email);
+    var url = "https://herokubackend-605c0ee15b4e.herokuapp.com/contactos/" + encodeURIComponent(email);
     request.open('GET', url);
     request.send();
 
@@ -44,4 +44,4 @@ function getForEmail() {
 }
 
 
-// var url = "http://localhost:8000/contactos/" + encodeURIComponent(email);
+
